8227c85712ea1b0d654f2d84feea05d0760d90e0
bkfleet1 <bkfleet1@gmail.com>
2022-05-02 15:52:03 -0500

    PWA & CSS mods

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
- mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/housePlantStoreDb', {
- 
-  useNewUrlParser: true,
+// mongoose.connect
+// (process.env.MONGODB_URI || 
+//'mongodb://localhost/housePlantStoreDb',{
+  mongoose.connect(
+    process.env.MONGODB_URI ||
+     'mongodb+srv://user:password999@cluster0.1jie0.mongodb.net/housePlantStoreDb',{
+  useNewUrlParser: true, 
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+}
+);
 
 module.exports = mongoose.connection;
